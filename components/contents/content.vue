6cd73924c7e7a935b951729164e168e0b53ed407
shouya <idb8bzputt@gmail.com>
2020-11-08 14:36:44 +0900

    header.vue content.vue を追加。

---
+++ components/contents/content.vue
@@ -0,0 +1,241 @@
+<template>
+  <table class="table">
+    <tr class="table__header">
+      <th class="table__th">対戦キラー</th>
+      <th class="table__th">使用率</th>
+      <th class="table__th">対戦数</th>
+      <th class="table__th">平均得点</th>
+      <th class="table__th">勝率</th>
+    </tr>
+
+    <tr class="table__tr">
+      <td class="table__td table__name">
+        <img class="table__image" src="~/assets/img/killer07.png" alt="" />
+        カニバル
+      </td>
+      <td class="table__td">12%</td>
+      <td class="table__td">12回</td>
+      <td class="table__td">20000</td>
+      <td class="table__td">40%</td>
+    </tr>
+    <tr class="table__tr">
+      <td class="table__td table__name">
+        <img class="table__image" src="~/assets/img/killer06.png" alt="" />
+        シェイプ
+      </td>
+      <td class="table__td">10%</td>
+      <td class="table__td">10回</td>
+      <td class="table__td">13000</td>
+      <td class="table__td">40%</td>
+    </tr>
+    <tr class="table__tr">
+      <td class="table__td table__name">
+        <img class="table__image" src="~/assets/img/killer01.png" alt="" />
+        レイス
+      </td>
+      <td class="table__td">10%</td>
+      <td class="table__td">10回</td>
+      <td class="table__td">17000</td>
+      <td class="table__td">60%</td>
+    </tr>
+    <tr class="table__tr">
+      <td class="table__td table__name">
+        <img class="table__image" src="~/assets/img/killer10.png" alt="" />
+        スピリット
+      </td>
+      <td class="table__td">7%</td>
+      <td class="table__td">7回</td>
+      <td class="table__td">8000</td>
+      <td class="table__td">20%</td>
+    </tr>
+    <tr class="table__tr">
+      <td class="table__td table__name">
+        <img class="table__image" src="~/assets/img/killer16.png" alt="" />
+        エクセキューショナー
+      </td>
+      <td class="table__td">7%</td>
+      <td class="table__td">7回</td>
+      <td class="table__td">12000</td>
+      <td class="table__td">30%</td>
+    </tr>
+    <tr class="table__tr">
+      <td class="table__td table__name">
+        <img class="table__image" src="~/assets/img/killer03.png" alt="" />
+        ヒルビリー
+      </td>
+      <td class="table__td">6%</td>
+      <td class="table__td">6回</td>
+      <td class="table__td">21000</td>
+      <td class="table__td">80%</td>
+    </tr>
+    <tr class="table__tr">
+      <td class="table__td table__name">
+        <img class="table__image" src="~/assets/img/killer02.png" alt="" />
+        ハグ
+      </td>
+      <td class="table__td">5%</td>
+      <td class="table__td">5回</td>
+      <td class="table__td">16000</td>
+      <td class="table__td">50%</td>
+    </tr>
+    <tr class="table__tr">
+      <td class="table__td table__name">
+        <img class="table__image" src="~/assets/img/killer04.png" alt="" />
+        ドクター
+      </td>
+      <td class="table__td">4%</td>
+      <td class="table__td">4回</td>
+      <td class="table__td">12000</td>
+      <td class="table__td">30%</td>
+    </tr>
+    <tr class="table__tr">
+      <td class="table__td table__name">
+        <img class="table__image" src="~/assets/img/killer05.png" alt="" />
+        ハントレス
+      </td>
+      <td class="table__td">6%</td>
+      <td class="table__td">6回</td>
+      <td class="table__td">19000</td>
+      <td class="table__td">70%</td>
+    </tr>
+    <tr class="table__tr">
+      <td class="table__td table__name">
+        <img class="table__image" src="~/assets/img/killer11.png" alt="" />
+        リージョン
+      </td>
+      <td class="table__td">6%</td>
+      <td class="table__td">6回</td>
+      <td class="table__td">16000</td>
+      <td class="table__td">60%</td>
+    </tr>
+
+    <tr class="table__tr">
+      <td class="table__td table__name">
+        <img class="table__image" src="~/assets/img/killer09.png" alt="" />
+        クラウン
+      </td>
+      <td class="table__td">6%</td>
+      <td class="table__td">6回</td>
+      <td class="table__td">16000</td>
+      <td class="table__td">60%</td>
+    </tr>
+
+    <tr class="table__tr">
+      <td class="table__td table__name">
+        <img class="table__image" src="~/assets/img/killer12.png" alt="" />
+        プレイグ
+      </td>
+      <td class="table__td">5%</td>
+      <td class="table__td">5回</td>
+      <td class="table__td">22000</td>
+      <td class="table__td">90%</td>
+    </tr>
+    <tr class="table__tr">
+      <td class="table__td table__name">
+        <img class="table__image" src="~/assets/img/killer13.png" alt="" />
+        ゴーストフェイス
+      </td>
+      <td class="table__td">5%</td>
+      <td class="table__td">5回</td>
+      <td class="table__td">14000</td>
+      <td class="table__td">60%</td>
+    </tr>
+    <tr class="table__tr">
+      <td class="table__td table__name">
+        <img class="table__image" src="~/assets/img/killer08.png" alt="" />
+        ピッグ
+      </td>
+      <td class="table__td">4%</td>
+      <td class="table__td">4回</td>
+      <td class="table__td">23000</td>
+      <td class="table__td">80%</td>
+    </tr>
+
+    <tr class="table__tr">
+      <td class="table__td table__name">
+        <img class="table__image" src="~/assets/img/killer15.png" alt="" />
+        鬼
+      </td>
+      <td class="table__td">3%</td>
+      <td class="table__td">3回</td>
+      <td class="table__td">7000</td>
+      <td class="table__td">40%</td>
+    </tr>
+
+    <tr class="table__tr">
+      <td class="table__td table__name">
+        <img class="table__image" src="~/assets/img/killer17.png" alt="" />
+        トラッパー
+      </td>
+      <td class="table__td">3%</td>
+      <td class="table__td">3回</td>
+      <td class="table__td">20000</td>
+      <td class="table__td">60%</td>
+    </tr>
+    <tr class="table__tr">
+      <td class="table__td table__name">
+        <img class="table__image" src="~/assets/img/killer18.png" alt="" />
+        ナース
+      </td>
+      <td class="table__td">3%</td>
+      <td class="table__td">3回</td>
+      <td class="table__td">20000</td>
+      <td class="table__td">70%</td>
+    </tr>
+    <tr class="table__tr">
+      <td class="table__td table__name">
+        <img class="table__image" src="~/assets/img/killer14.png" alt="" />
+        デモゴルゴン
+      </td>
+      <td class="table__td">2%</td>
+      <td class="table__td">2回</td>
+      <td class="table__td">16000</td>
+      <td class="table__td">50%</td>
+    </tr>
+  </table>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.table {
+  width: 80%;
+  margin: 0 auto 50px;
+  font-size: 1.4rem;
+  color: #fff;
+
+  &__th {
+    width: 20%;
+    padding-bottom: 20px;
+    text-align: center;
+    border-bottom: 1px solid #fff;
+    position: sticky;
+    top: 100px;
+    z-index: 1;
+    background-image: url('~@/assets/img/bg.jpg');
+    background-attachment: fixed;
+    background-size: cover;
+    background-position: center;
+  }
+
+  &__image {
+    width: 50px;
+    height: 50px;
+    display: flex;
+    margin: 0 auto 5px;
+  }
+
+  &__name {
+    font-size: 1.2rem;
+  }
+
+  &__td {
+    width: 20%;
+    padding: 15px 0 10px;
+    text-align: center;
+    border-bottom: 1px solid #fff;
+  }
+}
+</style>
