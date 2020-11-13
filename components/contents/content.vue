9190e43b0f8d3667b0bf32293668199c64bc6cfb
shouya <idb8bzputt@gmail.com>
2020-11-13 20:36:59 +0900

    content.vue を更新。

--- components/contents/content.vue
+++ components/contents/content.vue
@@ -2,273 +2,73 @@
   <table class="table">
     <tr class="table__header">
       <th class="table__th">対戦キラー</th>
-      <th class="table__th">使用率</th>
       <th class="table__th">対戦数</th>
       <th class="table__th">平均得点</th>
       <th class="table__th">勝率</th>
     </tr>
 
-    <tr class="table__tr">
-      <td class="table__td table__name">
-        <img
-          class="table__image"
-          src="~/assets/img/killer/killer07.png"
-          alt=""
-        />
-        カニバル
-      </td>
-      <td class="table__td">12%</td>
-      <td class="table__td">12回</td>
-      <td class="table__td">20000</td>
-      <td class="table__td">40%</td>
-    </tr>
-    <tr class="table__tr">
-      <td class="table__td table__name">
-        <img
-          class="table__image"
-          src="~/assets/img/killer/killer06.png"
-          alt=""
-        />
-        シェイプ
-      </td>
-      <td class="table__td">10%</td>
-      <td class="table__td">10回</td>
-      <td class="table__td">13000</td>
-      <td class="table__td">40%</td>
-    </tr>
-    <tr class="table__tr">
-      <td class="table__td table__name">
-        <img
-          class="table__image"
-          src="~/assets/img/killer/killer01.png"
-          alt=""
-        />
-        レイス
-      </td>
-      <td class="table__td">10%</td>
-      <td class="table__td">10回</td>
-      <td class="table__td">17000</td>
-      <td class="table__td">60%</td>
-    </tr>
-    <tr class="table__tr">
-      <td class="table__td table__name">
-        <img
-          class="table__image"
-          src="~/assets/img/killer/killer10.png"
-          alt=""
-        />
-        スピリット
-      </td>
-      <td class="table__td">7%</td>
-      <td class="table__td">7回</td>
-      <td class="table__td">8000</td>
-      <td class="table__td">20%</td>
-    </tr>
-    <tr class="table__tr">
-      <td class="table__td table__name">
-        <img
-          class="table__image"
-          src="~/assets/img/killer/killer16.png"
-          alt=""
-        />
-        エクセキューショナー
-      </td>
-      <td class="table__td">7%</td>
-      <td class="table__td">7回</td>
-      <td class="table__td">12000</td>
-      <td class="table__td">30%</td>
-    </tr>
-    <tr class="table__tr">
-      <td class="table__td table__name">
-        <img
-          class="table__image"
-          src="~/assets/img/killer/killer03.png"
-          alt=""
-        />
-        ヒルビリー
-      </td>
-      <td class="table__td">6%</td>
-      <td class="table__td">6回</td>
-      <td class="table__td">21000</td>
-      <td class="table__td">80%</td>
-    </tr>
-    <tr class="table__tr">
-      <td class="table__td table__name">
-        <img
-          class="table__image"
-          src="~/assets/img/killer/killer02.png"
-          alt=""
-        />
-        ハグ
-      </td>
-      <td class="table__td">5%</td>
-      <td class="table__td">5回</td>
-      <td class="table__td">16000</td>
-      <td class="table__td">50%</td>
-    </tr>
-    <tr class="table__tr">
-      <td class="table__td table__name">
-        <img
-          class="table__image"
-          src="~/assets/img/killer/killer04.png"
-          alt=""
-        />
-        ドクター
-      </td>
-      <td class="table__td">4%</td>
-      <td class="table__td">4回</td>
-      <td class="table__td">12000</td>
-      <td class="table__td">30%</td>
-    </tr>
-    <tr class="table__tr">
-      <td class="table__td table__name">
-        <img
-          class="table__image"
-          src="~/assets/img/killer/killer05.png"
-          alt=""
-        />
-        ハントレス
-      </td>
-      <td class="table__td">6%</td>
-      <td class="table__td">6回</td>
-      <td class="table__td">19000</td>
-      <td class="table__td">70%</td>
-    </tr>
-    <tr class="table__tr">
-      <td class="table__td table__name">
-        <img
-          class="table__image"
-          src="~/assets/img/killer/killer11.png"
-          alt=""
-        />
-        リージョン
-      </td>
-      <td class="table__td">6%</td>
-      <td class="table__td">6回</td>
-      <td class="table__td">16000</td>
-      <td class="table__td">60%</td>
-    </tr>
-
-    <tr class="table__tr">
-      <td class="table__td table__name">
-        <img
-          class="table__image"
-          src="~/assets/img/killer/killer09.png"
-          alt=""
-        />
-        クラウン
-      </td>
-      <td class="table__td">6%</td>
-      <td class="table__td">6回</td>
-      <td class="table__td">16000</td>
-      <td class="table__td">60%</td>
-    </tr>
-
-    <tr class="table__tr">
-      <td class="table__td table__name">
-        <img
-          class="table__image"
-          src="~/assets/img/killer/killer12.png"
-          alt=""
-        />
-        プレイグ
-      </td>
-      <td class="table__td">5%</td>
-      <td class="table__td">5回</td>
-      <td class="table__td">22000</td>
-      <td class="table__td">90%</td>
-    </tr>
-    <tr class="table__tr">
-      <td class="table__td table__name">
-        <img
-          class="table__image"
-          src="~/assets/img/killer/killer13.png"
-          alt=""
-        />
-        ゴーストフェイス
-      </td>
-      <td class="table__td">5%</td>
-      <td class="table__td">5回</td>
-      <td class="table__td">14000</td>
-      <td class="table__td">60%</td>
-    </tr>
-    <tr class="table__tr">
-      <td class="table__td table__name">
-        <img
-          class="table__image"
-          src="~/assets/img/killer/killer08.png"
-          alt=""
-        />
-        ピッグ
-      </td>
-      <td class="table__td">4%</td>
-      <td class="table__td">4回</td>
-      <td class="table__td">23000</td>
-      <td class="table__td">80%</td>
-    </tr>
-
-    <tr class="table__tr">
-      <td class="table__td table__name">
-        <img
-          class="table__image"
-          src="~/assets/img/killer/killer15.png"
-          alt=""
-        />
-        鬼
-      </td>
-      <td class="table__td">3%</td>
-      <td class="table__td">3回</td>
-      <td class="table__td">7000</td>
-      <td class="table__td">40%</td>
-    </tr>
-
-    <tr class="table__tr">
-      <td class="table__td table__name">
-        <img
-          class="table__image"
-          src="~/assets/img/killer/killer17.png"
-          alt=""
-        />
-        トラッパー
-      </td>
-      <td class="table__td">3%</td>
-      <td class="table__td">3回</td>
-      <td class="table__td">20000</td>
-      <td class="table__td">60%</td>
-    </tr>
-    <tr class="table__tr">
-      <td class="table__td table__name">
-        <img
-          class="table__image"
-          src="~/assets/img/killer/killer18.png"
-          alt=""
-        />
-        ナース
-      </td>
-      <td class="table__td">3%</td>
-      <td class="table__td">3回</td>
-      <td class="table__td">20000</td>
-      <td class="table__td">70%</td>
-    </tr>
-    <tr class="table__tr">
-      <td class="table__td table__name">
-        <img
-          class="table__image"
-          src="~/assets/img/killer/killer14.png"
-          alt=""
-        />
-        デモゴルゴン
-      </td>
-      <td class="table__td">2%</td>
-      <td class="table__td">2回</td>
-      <td class="table__td">16000</td>
-      <td class="table__td">50%</td>
-    </tr>
+    <Trapper />
+    <Wraith />
+    <Hillbilly />
+    <Nurse />
+    <Hag />
+    <Doctor />
+    <Huntress />
+    <Shape />
+    <Cannibal />
+    <Pig />
+    <Clown />
+    <Spirit />
+    <Legion />
+    <Plague />
+    <GhostFace />
+    <Demogorgon />
+    <Oni />
+    <Executioner />
   </table>
 </template>
 
 <script>
-export default {}
+import Trapper from '~/components/parts/killer-data/killer01-data'
+import Wraith from '~/components/parts/killer-data/killer02-data'
+import Hillbilly from '~/components/parts/killer-data/killer03-data'
+import Nurse from '~/components/parts/killer-data/killer04-data'
+import Hag from '~/components/parts/killer-data/killer05-data'
+import Doctor from '~/components/parts/killer-data/killer06-data'
+import Huntress from '~/components/parts/killer-data/killer07-data'
+import Shape from '~/components/parts/killer-data/killer08-data'
+import Cannibal from '~/components/parts/killer-data/killer09-data'
+import Pig from '~/components/parts/killer-data/killer10-data'
+import Clown from '~/components/parts/killer-data/killer11-data'
+import Spirit from '~/components/parts/killer-data/killer12-data'
+import Legion from '~/components/parts/killer-data/killer13-data'
+import Plague from '~/components/parts/killer-data/killer14-data'
+import GhostFace from '~/components/parts/killer-data/killer15-data'
+import Demogorgon from '~/components/parts/killer-data/killer16-data'
+import Oni from '~/components/parts/killer-data/killer17-data'
+import Executioner from '~/components/parts/killer-data/killer18-data'
+export default {
+  components: {
+    Trapper,
+    Wraith,
+    Hillbilly,
+    Nurse,
+    Hag,
+    Doctor,
+    Huntress,
+    Shape,
+    Cannibal,
+    Pig,
+    Clown,
+    Spirit,
+    Legion,
+    Plague,
+    GhostFace,
+    Demogorgon,
+    Oni,
+    Executioner,
+  },
+}
 </script>
 
 <style lang="scss" scoped>
@@ -290,24 +90,6 @@ export default {}
     background-attachment: fixed;
     background-size: cover;
     background-position: center;
-  }
-
-  &__image {
-    width: 50px;
-    height: 50px;
-    display: flex;
-    margin: 0 auto 5px;
-  }
-
-  &__name {
-    font-size: 1.2rem;
-  }
-
-  &__td {
-    width: 20%;
-    padding: 15px 0 10px;
-    text-align: center;
-    border-bottom: 1px solid #fff;
   }
 }
 </style>
